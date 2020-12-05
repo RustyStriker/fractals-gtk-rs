562fe1c55bcfbc6184499f2b882eccde7b2868bf
RustyStriker <avivr903@gmail.com>
2020-12-05 13:23:16 +0200

    some commit so i could revert to it

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,368 @@
 extern crate gtk;
 extern crate cairo;
-use gtk::prelude::*;
-use cairo::Context as Cairo;
 
-pub fn draw_circle(cairo : &Cairo, x : f64, y : f64, radius : f64){
+use cairo::Context as Cairo;
+use std::sync::{Weak,Mutex};
+use std::thread;
+use rand;
+
+fn draw_circle(cairo : &Cairo, x : f64, y : f64, radius : f64){
     cairo.set_line_width(radius / 10.0);
     cairo.arc(x,y,radius,0.0,360.0);
     cairo.stroke();
 }
 
-pub fn circle_fractals(cairo : &Cairo, size : (f64,f64), start : f64, min : f64){
-    circle_fractals_helper(cairo, size.0 / 2.0, size.1 / 2.0, start, min)
+pub fn circle_fractals(circles : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, size : (f64,f64), start : f64, min : f64){
+    {
+        let circles = match circles.upgrade() {
+            Some(c) => c,
+            _ => return (),
+        };
+        let mut mutex = circles.lock().unwrap();
+        mutex.clear();
+
+    }
+    thread::spawn(move || {
+        circle_fractals_helper(circles, size.0 / 2.0, size.1 / 2.0, start, min, size)
+    });
 }
 
-fn circle_fractals_helper(cairo : &Cairo, position : f64, y : f64, length : f64, min : f64){
+fn circle_fractals_helper(circles : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, position : f64, y : f64, length : f64, min : f64, size : (f64,f64)){
+    {
+        let color = (1.0 - (y / size.1), 1.0 - (min / length), 1.0 - (position / size.0));
+        let circle = Circle{
+            position : (position,y),
+            radius : length,
+            color,
+            thickness : length / 10.0,
+        };
+        thread::sleep(std::time::Duration::from_millis(10));
+        let upgrade = match circles.upgrade() {
+            Some(u) => u,
+            _ => return (),
+        };
+        let mut mutex = match upgrade.lock() {
+            Ok(lock) => lock,
+            _ => return (),
+        };
+        mutex.push(Box::new(circle));
+    }
     let new_len = length / 2.0;
     if new_len > min{
-        circle_fractals_helper(cairo, position + length, y, new_len, min);
-        circle_fractals_helper(cairo, position - length, y, new_len, min);
+        circle_fractals_helper(circles.clone(), position + length, y, new_len, min, size);
+        circle_fractals_helper(circles.clone(), position - length, y, new_len, min, size);
         
-        circle_fractals_helper(cairo, position, y + length, new_len, min);
-        circle_fractals_helper(cairo, position, y - length, new_len, min);
+        circle_fractals_helper(circles.clone(), position, y + length, new_len, min, size);
+        circle_fractals_helper(circles.clone(), position, y - length, new_len, min, size);
     }
-    cairo.set_source_rgb(0.0, 1.0 - (min / length), 0.0);
-    draw_circle(cairo, position, y, length);
+    
+}
 
+pub fn lines_fractal(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, size : (f64,f64), start : f64, min : f64){
+    let from = (size.0 / 2.0, size.1 / 2.0);
+    
+    // Clear the mutex 
+    {
+        let shapes = match shapes.upgrade() {
+            Some(c) => c,
+            _ => return (),
+        };
+        let mut mutex = shapes.lock().unwrap();
+        mutex.clear();
 
+    }
+
+    thread::spawn(move ||{
+        lines_fractal_helper(shapes.clone(), from, (from.0 + start, from.1), start, min, size);
+        lines_fractal_helper(shapes.clone(), from, (from.0 - start, from.1), start, min, size);
+    });
+}
+
+fn lines_fractal_helper(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, from : (f64,f64), to : (f64,f64), len : f64, min : f64, size : (f64,f64)) {
+    {
+        let color = (1.0 - (from.1 / size.1), 1.0 - (min / len), 1.0 - (from.0 / size.0));
+        let line = Line{
+            from,
+            to,
+            color,
+            thickness : len / 10.0,
+        };
+        thread::sleep(std::time::Duration::from_millis(1));
+        let upgrade = match shapes.upgrade() {
+            Some(u) => u,
+            _ => return (),
+        };
+        let mut mutex = match upgrade.lock() {
+            Ok(lock) => lock,
+            _ => return (),
+        };
+        mutex.push(Box::new(line));
+    }
+
+    let new_len = len * 0.7;
+    if new_len > min {
+        let dir = (sign(to.0 - from.0), sign(to.1 - from.1));
+        
+        lines_fractal_helper(shapes.clone(), to, (to.0 + dir.1 * new_len, to.1 + dir.0 * new_len), new_len, min, size);
+        lines_fractal_helper(shapes.clone(), to, (to.0 - dir.1 * new_len, to.1 - dir.0 * new_len), new_len, min, size);
+    }
+    
+}
+
+fn sign(num : f64) -> f64{
+    if num == 0.0{
+        0.0
+    }
+    else if num > 0.0 {
+        1.0
+    }
+    else {
+        -1.0
+    }
+}
+
+pub fn line_circle_fractals(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, size : (f64,f64), start : f64, min : f64){
+    // Clear the mutex 
+    {
+        let shapes = match shapes.upgrade() {
+            Some(c) => c,
+            _ => return (),
+        };
+        let mut mutex = shapes.lock().unwrap();
+        mutex.clear();
+
+    }
+    thread::spawn(move ||{
+        lines_circle_fractals_c_helper(shapes.clone(), (size.0 / 2.0,size.1/2.0), start, min, size);
+    });
+}
+
+fn lines_circle_fractals_c_helper(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, position : (f64,f64), length : f64, min : f64, size : (f64,f64)){
+    let new_len = length * 0.7;
+    if new_len > min {
+        let side_1 = (position.0 + length,position.1);
+        let side_2 = (position.0 - length, position.1);
+        let side_3 = (position.0, position.1 + length);
+        let side_4 = (position.0, position.1 - length);
+
+        lines_circle_fractals_l_helper(shapes.clone(), side_1, (side_1.0 + new_len,side_1.1), new_len, min, size);
+        lines_circle_fractals_l_helper(shapes.clone(), side_2, (side_2.0 - new_len,side_2.1), new_len, min, size);
+        lines_circle_fractals_l_helper(shapes.clone(), side_3, (side_3.0 ,side_3.1 + new_len), new_len, min, size);
+        lines_circle_fractals_l_helper(shapes.clone(), side_4, (side_4.0 ,side_4.1 - new_len), new_len, min, size);
+    }
+    let color = (1.0 - (position.1 / size.1), 1.0 - (min / length), 1.0 - (position.0 / size.0));
+    let circle = Circle{
+        position,
+        radius : length,
+        color,
+        thickness : length / 10.0,
+    };
+    thread::sleep(std::time::Duration::from_millis(1));
+    let upgrade = match shapes.upgrade() {
+        Some(u) => u,
+        _ => return (),
+    };
+    let mut mutex = match upgrade.lock() {
+        Ok(lock) => lock,
+        _ => return (),
+    };
+    mutex.push(Box::new(circle));
+}
+
+fn lines_circle_fractals_l_helper(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, from : (f64,f64), to : (f64,f64), len : f64, min : f64, size : (f64,f64)){
+    let new_len = len * 0.7 ;// 2.0;
+    if new_len > min{
+        lines_circle_fractals_c_helper(shapes.clone(), to, new_len, min, size);
+    }
+    let color = (1.0 - (from.1 / size.1), 1.0 - (min / len), 1.0 - (from.0 / size.0));
+    let line = Line{
+        from,
+        to,
+        color,
+        thickness : len / 10.0,
+    };
+    thread::sleep(std::time::Duration::from_millis(1));
+    let upgrade = match shapes.upgrade() {
+        Some(u) => u,
+        _ => return (),
+    };
+    let mut mutex = match upgrade.lock() {
+        Ok(lock) => lock,
+        _ => return (),
+    };
+    mutex.push(Box::new(line));
+}
+
+pub fn tree_fractal(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, size : (f64,f64), start : f64, branches : i32){
+    // Clear the mutex 
+    {
+        let shapes = match shapes.upgrade() {
+            Some(c) => c,
+            _ => return (),
+        };
+        let mut mutex = shapes.lock().unwrap();
+        mutex.clear();
+
+    }
+    thread::spawn(move ||{
+        tree_fractal_helper(shapes.clone(), (size.0 / 2.0, size.1), (size.0 / 2.0, size.1 - start), branches, size);
+    });
+}
+
+fn tree_fractal_helper(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, from : (f64,f64), to : (f64,f64), branches : i32, size : (f64,f64)){
+    {
+        let color = (1.0 - (from.1 / size.1), branches as f64 / 255.0, 1.0 - (from.0 / size.0));
+        let line = Line{
+            from, to, color,
+            thickness : 2.0,        
+        };
+        thread::sleep(std::time::Duration::from_millis(1));
+        let upgrade = match shapes.upgrade() {
+            Some(u) => u,
+            _ => return (),
+        };
+        let mut mutex = match upgrade.lock() {
+            Ok(lock) => lock,
+            _ => return (),
+        };
+        mutex.push(Box::new(line));
+    }
+    //println!("tree from {:?} to {:?} branches left {}",from,to,branches);
+    if branches > 0{
+        let len = ((to.0 - from.0).powi(2) + (to.1 - from.1).powi(2)).sqrt();
+        //let angle = ((to.1 - from.1) / len).asin(); 
+        //let angle = if (to.0 - from.0) < 0.0 { angle - 0.5 * 3.14 } else {angle};
+        let angle = (to.1 - from.1).atan2(to.0 - from.0);
+        let len = len * 0.7;
+
+        let to_1 = in_angle(len, -3.14 * 0.1 + angle, to);
+        let to_2 = in_angle(len, 3.14 * 0.1 + angle, to);
+        let to_4 = in_angle(len, -3.14 * 0.3 + angle, to);
+        let to_5 = in_angle(len, 3.14 * 0.3 + angle, to);
+
+        tree_fractal_helper(shapes.clone(), to, to_1, branches - 1, size);
+        tree_fractal_helper(shapes.clone(), to, to_2, branches - 1, size);
+        //tree_fractal_helper(cairo, to, to_3, branches - 1, size);
+        tree_fractal_helper(shapes.clone(), to, to_4, branches - 1, size);
+        tree_fractal_helper(shapes.clone(), to, to_5, branches - 1, size);
+
+    }
+    
+}
+
+pub fn random_tree_fractal(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, size : (f64,f64), start : f64, branches : i32){
+    // Clear the mutex 
+    {
+        let shapes = match shapes.upgrade() {
+            Some(c) => c,
+            _ => return (),
+        };
+        let mut mutex = shapes.lock().unwrap();
+        mutex.clear();
+
+    }
+    thread::spawn(move ||{
+        random_tree_fractal_helper(shapes.clone(), (size.0 / 2.0, size.1), (size.0 / 2.0, size.1 - start), branches, size);
+    });
+}
+
+fn random_tree_fractal_helper(shapes : Weak<Mutex<Vec<Box<dyn Shape + Send>>>>, from : (f64,f64), to : (f64,f64), branches : i32, size : (f64,f64)){
+    {
+        let color = (1.0 - (from.1 / size.1), branches as f64 / 255.0, 1.0 - (from.0 / size.0));
+        let line = Line{
+            from, to, color,
+            thickness : 2.0,        
+        };
+        thread::sleep(std::time::Duration::from_millis(1));
+        let upgrade = match shapes.upgrade() {
+            Some(u) => u,
+            _ => return (),
+        };
+        let mut mutex = match upgrade.lock() {
+            Ok(lock) => lock,
+            _ => return (),
+        };
+        mutex.push(Box::new(line));
+    }
+    //println!("tree from {:?} to {:?} branches left {}",from,to,branches);
+    if branches > 0{
+        let len = ((to.0 - from.0).powi(2) + (to.1 - from.1).powi(2)).sqrt();
+        //let angle = ((to.1 - from.1) / len).asin(); 
+        //let angle = if (to.0 - from.0) < 0.0 { angle - 0.5 * 3.14 } else {angle};
+        let angle = (to.1 - from.1).atan2(to.0 - from.0);
+        let len = len * 0.7;
+
+        let to_1 = in_angle(len, -3.14 * 0.2 + angle, to);
+        let to_2 = in_angle(len, 3.14 * 0.2 + angle, to);
+        let to_4 = in_angle(len, -3.14 * 0.4 + angle, to);
+        let to_5 = in_angle(len, 3.14 * 0.4 + angle, to);
+
+        for _ in 0..2{
+            let random = rand::random::<u8>() % 0b1111;
+            if random & 0b0001 != 0 {
+                random_tree_fractal_helper(shapes.clone(), to, to_1, branches - 1, size);
+            }
+            if random & 0b0010 != 0 {
+                random_tree_fractal_helper(shapes.clone(), to, to_2, branches - 1, size);
+            }
+            if random & 0b0100 != 0 {
+                random_tree_fractal_helper(shapes.clone(), to, to_4, branches - 1, size);
+            }
+            if random & 0b1000 != 0 {
+                random_tree_fractal_helper(shapes.clone(), to, to_5, branches - 1, size);
+            }
+        }
+    }
+    
+}
+
+fn in_angle(len : f64, angle : f64, from : (f64,f64)) -> (f64,f64){
+    (len * angle.cos() + from.0, len * angle.sin() + from.1)
+}
+
+pub trait Shape {
+    fn draw(&self, cairo : &Cairo);
+}
+
+#[derive(Debug, Clone)]
+pub struct Circle {
+    position : (f64,f64),
+    radius : f64,
+    color : (f64,f64,f64),
+    thickness : f64,
+}
+impl Circle {
+    pub fn new(position : (f64,f64), radius : f64) -> Circle{
+        Circle{
+            position,
+            radius,
+            color : (1.0,1.0,1.0),
+            thickness : 1.0,
+        }
+    }
+}
+impl Shape for Circle{
+    fn draw(&self, cairo : &Cairo){
+        cairo.set_source_rgb(self.color.0,self.color.1,self.color.2);
+        cairo.set_line_width(self.thickness);
+        cairo.arc(self.position.0,self.position.1,self.radius,0.0,7.0);
+        cairo.stroke();
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Line {
+    from : (f64,f64),
+    to : (f64,f64),
+    color : (f64,f64,f64),
+    thickness : f64,
+}
+impl Shape for Line{
+    fn draw(&self, cairo : &Cairo){
+        cairo.set_source_rgb(self.color.0,self.color.1,self.color.2);
+        cairo.set_line_width(self.thickness);
+        cairo.move_to(self.from.0, self.from.1);
+        cairo.line_to(self.to.0,self.to.1);
+        cairo.stroke();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate gtk;
 extern crate cairo;
 extern crate gdk_pixbuf;
-use gtk::prelude::*;
-use std::cell::RefCell;
-use std::rc::Rc;
-use cairo::Context as Cairo;
+extern crate gdk;
 
+use gtk::prelude::*;
+use std::sync::{Arc,Mutex};
+use fractals::Shape;
 
 fn main() {
     if gtk::init().is_err(){
@@ -17,115 +17,25 @@ fn main() {
     let builder = gtk::Builder::from_string(glade_src);
     let window : gtk::Window = builder.get_object("main_window").unwrap();
 
-    // num_x pretty much all of it
-    {
-        let num_x : gtk::Entry = builder.get_object("num_x").unwrap();
-        let add : gtk::Button = builder.get_object("num_x_add").unwrap();
-        add.connect_clicked(move |_| {
-            let number : String = num_x.get_text().into();
-            let number : i32 = match number.parse() {
-                Ok(v) => v,
-                Err(_) => 0
-            };
-
-            let number = number + 10;
-            num_x.set_text(&number.to_string());
-        });
-        let num_x : gtk::Entry = builder.get_object("num_x").unwrap();
-        let sub : gtk::Button = builder.get_object("num_x_sub").unwrap();
-        sub.connect_clicked(move |_| {
-            let number : String = num_x.get_text().into();
-            let number : i32 = match number.parse() {
-                Ok(v) => v,
-                Err(_) => 0
-            };
-
-            let number = if number - 10 < 1 { 1 } else { number - 10 };
-            num_x.set_text(&number.to_string());
-        });
-    }
-    let num_x : gtk::Entry = builder.get_object("num_x").unwrap();
-    // let drawing : gtk::DrawingArea = builder.get_object("drawing").unwrap();
-
-    // num_x.connect_changed(move |_| {
-    //     drawing.queue_draw();
-    // });
-    // Do not accept any non numeric char
-    num_x.connect_insert_text(|s, c, _| {
-        if c.chars().into_iter().any(|c| !c.is_numeric() && c != '-'){
-            println!("num_x: {} is not a number",c);
-            s.stop_signal_emission("insert_text");
-        }
-    });
-    // num_y - basically a copy of num_x
-    {
-        let num_y : gtk::Entry = builder.get_object("num_y").unwrap();
-        let add : gtk::Button = builder.get_object("num_y_add").unwrap();
-        add.connect_clicked(move |_| {
-            let number : String = num_y.get_text().into();
-            let number : i32 = match number.parse() {
-                Ok(v) => v,
-                Err(_) => 0
-            };
-
-            let number = number + 10;
-            num_y.set_text(&number.to_string());
-        });
-        let num_y : gtk::Entry = builder.get_object("num_y").unwrap();
-        let sub : gtk::Button = builder.get_object("num_y_sub").unwrap();
-        sub.connect_clicked(move |_| {
-            let number : String = num_y.get_text().into();
-            let number : i32 = match number.parse() {
-                Ok(v) => v,
-                Err(_) => 0
-            };
-
-            let number = if number - 10 < 1 { 1 } else { number - 10 };
-
-            num_y.set_text(&number.to_string());
-        });
-    }
-    let num_y : gtk::Entry = builder.get_object("num_y").unwrap();
-    // let drawing : gtk::DrawingArea = builder.get_object("drawing").unwrap();
-    // num_y.connect_changed(move |_| {
-    //     drawing.queue_draw();
-    // });
-    // Do not accept any non numeric char
-    num_y.connect_insert_text(|s, c, _| {
-        if c.chars().into_iter().any(|c| !c.is_numeric() && c != '-'){
-            println!("num_y: {} is not a number",c);
-            s.stop_signal_emission("insert_text");
-        }
-    });
-
-    // Color picker stuff
-    // let color = connect_color_picker(&builder);
+    build_ui(&builder);
     
-    let flipper = Rc::new(RefCell::new(false));
-    let flipper_copy = Rc::downgrade(&flipper);
 
+    let shapes : Arc<Mutex<Vec<Box<dyn Shape + Send>>>> = Arc::new(Mutex::new(vec!()));
     
+
     let refresh : gtk::Button = builder.get_object("redraw").unwrap();
     let drawing : gtk::DrawingArea = builder.get_object("drawing").unwrap();
     
     let da : gtk::DrawingArea = builder.get_object("drawing").unwrap();
-    let drawing_size = da.get_allocation();
-    //let pixbuf = gdk_pixbuf::Pixbuf::new(gdk_pixbuf::Colorspace::Rgb, false, 8, drawing_size.width, drawing_size.height);
-    //let pixbuf = Rc::new(pixbuf.unwrap());
-    //let pix_copy = Rc::downgrade(&pixbuf);
-    let surface = cairo::ImageSurface::create(cairo::Format::ARgb32, drawing_size.width, drawing_size.height).unwrap();
-    let surface = Rc::new(surface);
-    let sur_copy = Rc::downgrade(&surface);
 
+    let num_x : gtk::Entry = builder.get_object("num_x").unwrap();
+    let num_y : gtk::Entry = builder.get_object("num_y").unwrap();
+    let type_picker : gtk::ComboBox = builder.get_object("type_picker").unwrap();
 
+    let shapes_down = Arc::downgrade(&shapes);
     refresh.connect_clicked(move |_| {
-        let flippy = flipper_copy.upgrade().unwrap();
-        let mut flippy = flippy.borrow_mut();
-        *flippy = true;
-
+        let size = (drawing.get_allocated_width() as f64, drawing.get_allocated_height() as f64);
         
-        let cairo = cairo::Context::new(&surface);
-
         let start : f64 = match num_x.get_text().parse() {
             Ok(v) => v,
             Err(_) => 0.0
@@ -135,41 +45,37 @@ fn main() {
             Err(_) => 0.0
         };
 
-        fractals::circle_fractals(&cairo, (surface.get_width() as f64, surface.get_height() as f64),start, start / min);
-        println!("render");
-
+        match type_picker.get_active(){
+            Some(0) => fractals::circle_fractals(shapes_down.clone(), size,start, start / min),
+            Some(1) => fractals::lines_fractal(shapes_down.clone(), size, start, start / min),
+            Some(2) => fractals::line_circle_fractals(shapes_down.clone(), size, start, start / min),
+            Some(3) => fractals::tree_fractal(shapes_down.clone(), size, start, min as i32),
+            Some(4) => fractals::random_tree_fractal(shapes_down.clone(), size, start, min as i32),
+            _ => println!("NOT IMPLEMENTED")
+        };
+        
         drawing.queue_draw();
     });
     // draw the stuff
     da.connect_draw(move |_da, cairo| {
-        //let color = color.borrow();
-        //cairo.set_source_rgb(color.0, color.1, color.2);
-        
-        // let pos_x : f64 = match num_x.get_text().parse() {
-        //     Ok(v) => v,
-        //     Err(_) => 0.0
-        // };
-        // let pos_y : f64 = match num_y.get_text().parse() {
-        //     Ok(v) => v,
-        //     Err(_) => 0.0
-        // };
-        // let mut flipper = flipper.borrow_mut();
-        // if *flipper{
-        //     //fractals::circle_fractals(da,cairo,pos_x,pos_x / pos_y);
-        //     *flipper = false;
-        // }
-        // else{
-            
-        // }
-        let surface = sur_copy.upgrade().unwrap();
-        cairo.set_source_surface(&surface, 0.0, 0.0);
-        cairo.paint();
-        println!("Drawing");
+        let shapes = shapes.try_lock();
+        match shapes {
+            Ok(shapes) => {
+                let vec = &*shapes;
+                vec.iter().for_each(|c | c.draw(&cairo));
 
+            },
+            _ => (),
+        };
 
         gtk::Inhibit(true)
     });
     
+    // set the 1 / fps
+    glib::timeout_add_local(100, move ||{
+        da.queue_draw();
+        glib::Continue(true)
+    });
 
     window.connect_destroy(|_| {
         gtk::main_quit();
@@ -178,22 +84,62 @@ fn main() {
     gtk::main();
 }
 
+fn build_ui(builder : &gtk::Builder){
+    connect_ticker(&builder, "num_x", 1);
+    connect_ticker(&builder, "num_y",1);
 
+    // Set up combo box
+    let type_picker : gtk::ComboBox = builder.get_object("type_picker").unwrap();
+    let cell = gtk::CellRendererText::new();
+    type_picker.pack_start(&cell, false);
+    type_picker.add_attribute(&cell, "text", 0);
+    // set up the choices
+    store_setup(&type_picker);
+}
 
-fn connect_color_picker(builder : &gtk::Builder) -> Rc<RefCell<(f64,f64,f64)>>{
-    let color_dia : gtk::Button = builder.get_object("color_dialog").unwrap();
-    let color = Rc::new(RefCell::new((0.0,0.0,0.0)));
-    let color_copy = Rc::downgrade(&color);
-    let color_picker : gtk::ColorChooserDialog = builder.get_object("color_picker").unwrap();
-    color_picker.connect_hide(move |s|{
-        let color = color_copy.upgrade().unwrap();
-        let mut color = color.borrow_mut();
-        let r = s.get_rgba();
-        (*color) = (r.red, r.green, r.blue);
+fn store_setup(picker : &gtk::ComboBox){
+    let store = gtk::ListStore::new(&[glib::Type::String]);
+
+    store.insert_with_values(None, &[0], &[&"circles"]);
+    store.insert_with_values(None, &[0], &[&"lines"]);
+    store.insert_with_values(None, &[0], &[&"l+c"]);
+    store.insert_with_values(None, &[0], &[&"Tree"]);
+    store.insert_with_values(None, &[0], &[&"Random Tree"]);
+
+    picker.set_model(Some(&store));
+}
+
+fn connect_ticker(builder : &gtk::Builder, name : &str, step : i32){
+    let num : gtk::Entry = builder.get_object(name).expect(&format!("could not find {} in builder",name));
+    let add : gtk::Button = builder.get_object(&format!("{}_{}",name,"add")).expect(&format!("could not find  {}_add", name));
+    add.connect_clicked(move |_| {
+        let number : String = num.get_text().into();
+        let number : i32 = match number.parse() {
+            Ok(v) => v,
+            Err(_) => 0
+        };
+
+        let number = number + step;
+        num.set_text(&number.to_string());
     });
-    color_dia.connect_clicked(move |_| {
-        let _ = color_picker.run();
-        color_picker.hide();
+    let num : gtk::Entry = builder.get_object(name).unwrap(); // no need to expect because it will be cought on the first call
+    let sub : gtk::Button = builder.get_object(&format!("{}_{}",name,"sub")).expect(&format!("could not find  {}_sub", name));
+    sub.connect_clicked(move |_| {
+        let number : String = num.get_text().into();
+        let number : i32 = match number.parse() {
+            Ok(v) => v,
+            Err(_) => 0
+        };
+
+        let number = if number - step < 1 { 1 } else { number - step };
+        num.set_text(&number.to_string());
     });
-    color
+    
+    let num : gtk::Entry = builder.get_object(name).unwrap();
+    num.connect_insert_text(move |s, c, _| {
+        if c.chars().into_iter().any(|c| !c.is_numeric() && c != '-'){
+            println!("ERROR: {} is not a number",c);
+            s.stop_signal_emission("insert_text");
+        }
+    });
 }
